dd431b9328b9aa8de4672306b9cbea48d47fbda2
daoptph23912 <daoptph23912@fpt.edu.vn>
2024-07-27 09:40:21 +0700

    updated

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,19 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 
-import 'firebase/analytics';
-import 'firebase/auth';
-import 'firebase/firestore';
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyBRpDZ63mVfrzmzTjHFjRssP4cOvw_jc04',
-  authDomain: 'chat-app-c975a.firebaseapp.com',
-  projectId: 'chat-app-c975a',
-  storageBucket: 'chat-app-c975a.appspot.com',
-  messagingSenderId: '942648297216',
-  appId: '1:942648297216:web:d8c4321ba778c8808f95e5',
-  measurementId: 'G-01WLMKYFFM',
+  apiKey: "AIzaSyAXOlAz0ngBUnjLt3oDt5aetpfU2zfvjLc",
+  authDomain: "thanhdao-3c5ff.firebaseapp.com",
+  databaseURL:
+    "https://thanhdao-3c5ff-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "thanhdao-3c5ff",
+  storageBucket: "thanhdao-3c5ff.appspot.com",
+  messagingSenderId: "927313929250",
+  appId: "1:927313929250:web:257378888adb3963cbd67b",
+  measurementId: "G-1NR3V6F565",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -20,7 +22,7 @@ firebase.analytics();
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-if (window.location.hostname === 'localhost') {
+if (window.location.hostname === "localhost") {
   // auth.useEmulator('http://localhost:9099');
   // db.useEmulator('localhost', '8080');
 }
